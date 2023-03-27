2e8af53760041e51aaf1f7851da3a622b1778818
WantedStream <arturyosher76@gmail.com>
2023-03-27 16:23:13 +0300

    added more

--- chessboard.rb
+++ chessboard.rb
@@ -16,12 +16,18 @@ class ChessBoard
      end
 
      def Move(piece,row,col)
-        RemovePosition(piece.positionX,piece.positionY)
+      raise "error, out of bounds!" if(IsOutOfBounds(row,col))
+      raise "cant move!" if !piece.CanMove?(row,col)
+        RemovePosition(piece.row,piece.col)
         SetPosition(piece,row,col)
      end
+
+
      def SetPosition(piece,row,col)
         raise "error, out of bounds!" if(IsOutOfBounds(row,col))
         @chessboard[row][col]=piece
+        piece.row=row
+        piece.col=col
      end
      def RemovePosition(row,col)
         raise "error, out of bounds!" if(IsOutOfBounds(row,col))
@@ -30,14 +36,28 @@ class ChessBoard
 end
 
 class Knight
+   attr_accessor :row, :col
+   def initialize(row,col)
+      @row=row
+      @col=col
+   end
 
-    def initialize()
-        @chessboard = Array.new(8) { Array.new(8)}
-     end
+   def CanMove?(row,col)
+      if(row==@row+2)
+         return true if(col==@col+1||col==@col-1)      
+      elsif(row==@row+1)
+         return true if(col==@col+2||col==@col-2)
+      elsif(row==@row-1)
+         return true if(col==@col+2||col==@col-2)
+      elsif(row==@row-2)
+         return true if(col==@col+1||col==@col-1)
+      end
+      return false
+   end 
   
      
 end
 cs = ChessBoard.new()
-p cs.GetPiece(0,0)
-cs.SetPosition("knight",0,0)
-p cs.GetPiece(0,0)
+knight=Knight.new(0,0)
+cs.SetPosition(knight,0,0)
+
