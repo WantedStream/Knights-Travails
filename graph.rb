ecfa56d916ca150d81ea5c10238bfe1e7c784af4
WantedStream <arturyosher76@gmail.com>
2023-03-28 15:03:41 +0300

    add more stuffffff

--- graph.rb
+++ graph.rb
@@ -1,13 +1,10 @@
 class DirectedGraphNode
 
-    def initialize(value,g)
+    def initialize(value,g=[])
         @value = value
-        @graph=[g];
+        @graph=g;
     end
-    def initialize(value)
-        @value = value
-        @graph=[];
-    end
+
     def AddNext(g)
         @graph.push(g)
     end
@@ -15,4 +12,38 @@ class DirectedGraphNode
     def SetValue(value)
         @value=value
     end
+
+    def GetValue()
+        return @value
+    end
+
+    def GetNexts()
+        return @graph
+    end
+
+    def PrintGraph(directedgn=self)
+
+      #  queue=[directedgn]
+       # while(queue.size>0)
+       #     head=queue.shift
+       #     p head.GetValue()
+
+       # end
+    end
+
+    def GetHeight(directedgn=self,v2)
+        
+        if(directedgn==nil)
+            return 0
+        end
+        return GetMax(directedgn.GetNexts)+1;
+    end
+    def GetMax(nexts)
+        max=nexts[0]
+        for x in nexts do
+            max=[GetHeight(x),GetHeight(max)].max
+        end
+        return max
+    end
+
 end
